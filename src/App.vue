736e1a6978a7329b09db6973a9230981f2952e13
Liuxiaochen <903531184@qq.com>
2021-04-04 19:33:40 +0800

    删除冗余代码

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
   <ProgressBar/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import ProgressBar from "@/components/ProgressBar";
 
 export default {
   name: 'App',
   components: {
     ProgressBar,
-    HelloWorld
   }
 }
 </script>
